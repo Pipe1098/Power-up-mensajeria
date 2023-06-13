c958e44a3c1409ab8b344690d9ffbe9a1cc40a5f
Pipe1098 <pipe1e@hotmail.com>
2023-06-12 19:04:46 -0500

    Agregando variables de entorno

--- src/main/java/com/pragma/messengerservice/adapters/driven/mysql/adapter/MessageTwilioAdapter.java
+++ src/main/java/com/pragma/messengerservice/adapters/driven/mysql/adapter/MessageTwilioAdapter.java
@@ -7,27 +7,27 @@ import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 
 
 @RequiredArgsConstructor
 public class MessageTwilioAdapter implements IMessageTwilio {
 
-    private String ACCOUNT_SID;
-
-    private String AUTH_TOKEN;
-
-    private String PHONE_NUMBER;
+    @Value("${TWILIO_SID}")
+    private String sid;
+    @Value("${TWILIO_TOKEN}")
+    private String token;
+    @Value("${TWILIO_PHONE_NUMBER}")
+    private String phone;
 
 
     @Override
     public void sendSmsMessage(MessageModel messageModel) {
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-        Message message = Message.creator(
-                new PhoneNumber(messageModel.getPhone()),
-                new PhoneNumber(PHONE_NUMBER),
+        Twilio.init(sid, token);
+         Message.creator(
+                new PhoneNumber(messageModel.getPhone()),//num destino
+                new PhoneNumber(phone),// num origen
                 messageModel.getMessage()).
                         create();
-
-        System.out.println(message.getSid());
     }
 }
